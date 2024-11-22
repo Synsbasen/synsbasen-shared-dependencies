168f1d039a2067a95cd4ed1d24274e7a3deb46c1
Tobias Knudsen <tobias.knudsen@gmail.com>
2024-11-22 09:19:02 +0100

    Refactor

--- lib/railtie.rb
+++ lib/railtie.rb
@@ -4,7 +4,6 @@ module SharedDependenciesGem
   class Railtie < Rails::Railtie
     # Dynamically load all initializers
     initializer 'shared_dependencies_gem.load_initializers' do
-      puts Dir[File.expand_path('../initializers/*.rb', __FILE__)].count
       Dir[File.expand_path('../initializers/*.rb', __FILE__)].sort.each do |initializer|
         require initializer
       end
